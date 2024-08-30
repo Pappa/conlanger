b9452fabfb3617492136c86454871e0108c7508b
Pappa <dr.pappa@gmail.com>
2024-08-31 00:28:59 +0100

    Unit tests

--- src/conlanger/tools/Lexicon.py
+++ src/conlanger/tools/Lexicon.py
@@ -12,17 +12,19 @@ class Lexicon:
         nasals=None,
         word_list=None,
         style=None,
-        weirdness=0.5,
-        seed=None
+        probability=0.5,
+        seed=None,
+        max_syllables=3
     ) -> None:
+        random.seed(seed)
         self._phonemes = {
             "C": consonants, "V": vowels, "G": glides, "N": nasals
         }
         self._syllable_structure = self._parse_syllable_structure(syllable_structure)
         self._word_list = word_list
         self._style = style
-        self._weirdness = weirdness
-        random.seed(seed)
+        self._probability = probability
+        self._max_syllables = max_syllables
 
 
     def _parse_syllable_structure(self, structure: str) -> list[dict]:
@@ -37,6 +39,10 @@ class Lexicon:
     def create_syllable(self) -> str:
         syllable = ""
         for c in self._syllable_structure:
-            if c.optional and random.random() < self._weirdness:
-                pass
+            if not c["optional"] or (c["optional"] and random.random() < self._probability):
+                syllable += random.choice(self._phonemes[c["type"]])
 
+        return syllable
+    
+    def create_word(self) -> str:
+        return "".join([self.create_syllable() for _ in range(random.randint(1, self._max_syllables))])
--- tests/conlanger/tools/test_Lexicon.py
+++ tests/conlanger/tools/test_Lexicon.py
@@ -20,3 +20,25 @@ def test_Lexicon_syllable_structure(structure):
 def test_Lexicon_syllable_structure_errors(structure):
     with pytest.raises(ValueError, match="Invalid syllable structure"):
         Lexicon(syllable_structure=structure)
+
+@pytest.mark.parametrize(
+    "structure, consonants, vowels, glides, nasals, expected",
+    [("(C)V(C)(G)(N)", ['b', 'd', 'k', 'l', 'p'], ['i', 'u', 'ə'], ['j', 'w', 'ɹ'], ['m', 'n', 'ŋ'], "upw"),
+     ("(C)V(C)", ['k', 'l', 'p'], ['i', 'ə'], ['w'], ['ŋ'], "əp"),],
+)
+def test_Lexicon_create_syllable(structure, consonants, vowels, glides, nasals, expected):
+    lex = Lexicon(syllable_structure=structure, consonants=consonants, vowels=vowels, glides=glides, nasals=nasals, seed=0)
+    syllable = lex.create_syllable()
+
+    assert syllable == expected
+
+@pytest.mark.parametrize(
+    "structure, consonants, vowels, glides, nasals, max_syllables, expected",
+    [("(C)V(C)(G)(N)", ['b', 'd', 'k', 'l', 'p'], ['i', 'u', 'ə'], ['j', 'w', 'ɹ'], ['m', 'n', 'ŋ'], 2, "upwdəd"),
+     ("(C)V(C)", ['k', 'l', 'p'], ['i', 'ə'], ['w'], ['ŋ'], 3, "əplək"),],
+)
+def test_Lexicon_create_syllable(structure, consonants, vowels, glides, nasals, max_syllables, expected):
+    lex = Lexicon(syllable_structure=structure, consonants=consonants, vowels=vowels, glides=glides, nasals=nasals, seed=0, max_syllables=max_syllables)
+    word = lex.create_word()
+
+    assert word == expected
